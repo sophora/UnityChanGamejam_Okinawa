17ecef86e8310d292cbce9351babb265fe64b192
Takafumi Enji <takafumi_enji@summer-time-studio.com>
2014-06-14 17:48:16 +0900

    攻撃モーションを実装

--- UnityProject/Assets/Scripts/AttackBase.cs
+++ UnityProject/Assets/Scripts/AttackBase.cs
@@ -5,4 +5,21 @@ public class AttackBase : MonoBehaviour
 {
 	public float Damage;
 	public float ShotSpeed;
+	public float LifeTime;
+	public float CoolDown;
+
+	protected virtual void Update()
+	{
+		mSeconds += Time.deltaTime;
+
+		if (LifeTime > 0.0f)
+		{
+			if (mSeconds >= LifeTime)
+			{
+				Destroy(gameObject);
+			}
+		}
+	}
+
+	private float mSeconds;
 }
