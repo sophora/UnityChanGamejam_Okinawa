e462964677bf583aa6c9287898e878c974ac9459
HIROKI KINJO <e135755@HIROKI-no-MacBook-Pro.local>
2014-06-14 16:50:29 +0900

    no message

--- UnityProject/Assets/Scripts/EnemyController.cs
+++ UnityProject/Assets/Scripts/EnemyController.cs
@@ -12,9 +12,34 @@ public class EnemyController : MonoBehaviour
 
 	void FixedUpdate()
 	{
+		transform.LookAt(Vector3.zero);
+
 		Vector3 aPosition = Vector3.zero - rigidbody.position;
 
 		rigidbody.position = rigidbody.position + (aPosition.normalized * Speed * Time.fixedDeltaTime);
 
+		if(enemyhealth == 0){
+			Destroy(gameObject);
+		}
 	}
+
+	void OnTriggerEnter(Collider other){
+
+		Destroy(gameObject);
+	}
+	public float attackpower = 1.0f;
+	public float enemyhealth = 1.0f;
+
+	void OntrriggerEnter(Collider other){
+		AttackBase aAttack = other.gameObject.GetComponent<AttackBase>();
+		if(aAttack !=null){
+			enemyhealth = enemyhealth - aAttack.Damage;
+
+		}
+		
+	}
+
+
+
 }
+
--- UnityProject/Assets/Scripts/EnemySpawner.cs
+++ UnityProject/Assets/Scripts/EnemySpawner.cs
@@ -12,11 +12,19 @@ public class EnemySpawner : MonoBehaviour
 		//Instantiate(Enemy_001, , Quaternion.identity);
 
 	}
-
+	private float second;
 	// Update is called once per frame
 	void Update ()
 	{
-
+		second += Time.deltaTime ;
+		if(second > 5.0f){
+		float rat = Random.Range(0.0f,360.0f);   
+		float x = 40.0f * Mathf.Cos(Mathf.PI * rat / 180.0f);
+		float z = 40.0f* Mathf.Sin(Mathf.PI * rat / 180.0f);
+			
+		Instantiate ( Enemy_001, new Vector3(x,3,z),Quaternion.identity );
+	    second = 0.0f; 
+		} 
 
 	}
 }
