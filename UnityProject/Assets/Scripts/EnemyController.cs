558ec95db27fb44c2a05c0c1e968cddd91900278
Takafumi Enji <takafumi_enji@summer-time-studio.com>
2014-06-14 13:33:43 +0900

    Unityプロジェクトを作成

---
+++ UnityProject/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour
+{
+	public float Speed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	void FixedUpdate()
+	{
+		Vector3 aPosition = Vector3.zero - rigidbody.position;
+
+		rigidbody.position = rigidbody.position + (aPosition.normalized * Speed * Time.fixedDeltaTime);
+
+	}
+}
---
+++ UnityProject/Assets/Scripts/UnityChanController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof (Animator))]
+[RequireComponent(typeof (CapsuleCollider))]
+[RequireComponent(typeof (Rigidbody))]
+
+public class UnityChanController : MonoBehaviour
+{
+
+	private void FixedUpdate()
+	{
+		if(Input.GetButton("Fire1"))
+		{
+
+		}
+		else if(Input.GetButton("Fire2"))
+		{
+
+		}
+	}
+
+
+
+	private void ResetCollider()
+	{
+		mCapsuleCollider.height = mOrgColHight;
+		mCapsuleCollider.center = mOrgVectColCenter;
+	}
+
+	private void Start()
+	{
+		mAnimator         = GetComponent<Animator>();
+		mCapsuleCollider  = GetComponent<CapsuleCollider>();
+		mRigidbody        = GetComponent<Rigidbody>();
+		mCameraObject     = GameObject.FindWithTag("MainCamera");
+		mOrgColHight      = mCapsuleCollider.height;
+		mOrgVectColCenter = mCapsuleCollider.center;
+    }
+
+	private Animator          mAnimator;
+	private AnimatorStateInfo mCurrentBaseState;
+	private CapsuleCollider   mCapsuleCollider;
+	private Rigidbody         mRigidbody;
+	private GameObject        mCameraObject;
+	private float 			  mOrgColHight;
+	private Vector3           mOrgVectColCenter;
+}
